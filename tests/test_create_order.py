6743d90be4fd2ae927aab3a2c026ee5429d511ad
Леонид Пионер <peon-russ@mail.ru>
2026-03-06 13:20:19 +0300

    В test_create_order добавлена проверка создания заказа без авторизации

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -39,7 +39,7 @@ class TestCreateOrder:
         assert create_order.json()['success'] == False
 
     @allure.title('Проверка создания заказа c неверным хешем ингредиента')
-    def test_incorrect_login(self, user):
+    def test_incorrect_ingredient(self, user):
         """
         Регистрируем пользователя, создаём заказ с неверным хешем ингредиента.
         """
@@ -53,3 +53,21 @@ class TestCreateOrder:
             Url.ORDERS, data=order.payload, headers=user.headers)
 
         assert create_order.status_code == 500
+
+    @allure.title('Проверка создания заказа без авторизации')
+    def test_create_order_without_autorisation(self, user):
+        """
+        Регистрируем пользователя, создаём и отправляем заказ без заголовка авторизации.
+        В документации указано, что должна происходить переадресация на маршрут login.
+        По факту приходит успешный ответ 200 OK.
+        """
+        user.get_ingredients()
+        order = Order()
+        order.payload["ingredients"] = create_ingredient_list(
+            user.ingredients, 2)
+
+        create_order = requests.post(
+            Url.ORDERS, data=order.payload)
+
+        assert create_order.status_code == 303
+        assert create_order.check_url() == Url.LOGIN
